eea81dc1a4ec0aae4aca287b8766961d65f49b24
Antony Ishchenko <antony.ishchenko@gmail.com>
2025-06-01 22:45:49 +0300

    initial

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { MessagesModule } from 'src/messages/messages.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(MessagesModule);
+  await app.listen(process.env.PORT ?? 3000);
+}
+
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+bootstrap();
